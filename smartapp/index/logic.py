dd73b6a7c930c264b86a80b05a16a00c386cdbea
kadrisch <102146225+kadrisch@users.noreply.github.com>
2022-11-19 13:58:39 +0200

    Added logic for lowercase, remove new line and extra space, count characters, remove stop words

--- smartapp/index/logic.py
+++ smartapp/index/logic.py
@@ -1,4 +1,9 @@
 import string
+import nltk
+nltk.download('stopwords')
+nltk.download('punkt')
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 
 
 def punctuation_removal(text):
@@ -10,3 +15,42 @@ def punctuation_removal(text):
 
 def text_upper(text):
     return text.upper()
+
+
+def text_lower(text):
+    return text.lower()
+
+
+def remove_new_line(text):
+    text = ''.join(text.splitlines())
+    return text
+
+
+def remove_extra_space(text):     # removes duplicate whitespaces and newline characters
+    text = ' '.join(text.split())
+    return text
+
+
+def count_characters(text):
+    text = len(text)
+    return text
+
+
+def spell(text):
+    pass
+
+
+def wiki(text):
+    pass
+
+
+def remove_stop_words(text):
+    stop_words = set(stopwords.words('english'))
+    word_tokens = word_tokenize(text)
+    filtered_sentence = []
+
+    for w in word_tokens:
+        if w.lower() not in stop_words:
+            filtered_sentence.append(w)
+    text = ' '.join(filtered_sentence)
+    return text
