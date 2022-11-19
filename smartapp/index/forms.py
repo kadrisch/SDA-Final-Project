9999e21d6b68958d44f600658475a62aff1106ca
ademilua <tolesy2003@outlook.com>
2022-11-19 10:39:07 +0200

    connect logic

--- smartapp/index/forms.py
+++ smartapp/index/forms.py
@@ -2,5 +2,14 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
 
-class TextArea(forms.Form):
-    text = forms.CharField(widget=forms.Textarea(attrs={"rows":5, "cols":20, "placeholder": "Enter your text here"}), label='')
+class TextForm(forms.Form):
+    textarea = forms.CharField(max_length=500, widget=forms.Textarea, required=True)
+    remove_punctuations = forms.BooleanField(required=False)
+    upper_case = forms.BooleanField(required=False)
+    lower_case = forms.BooleanField(required=False)
+    new_line_remove = forms.BooleanField(required=False)
+    extra_space_remove = forms.BooleanField(required=False)
+    count_characters = forms.BooleanField(required=False)
+    spell_check = forms.BooleanField(required=False)
+    generate_word_summary = forms.BooleanField(required=False)
+    remove_stop_words = forms.BooleanField(required=False)
